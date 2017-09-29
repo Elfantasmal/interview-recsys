db4f4d87f6ed58bb5ea3267355a356fd18d8dbba
parker <rrdssfgcs@163.com>
2017-09-29 22:01:08 +0800

    ADD:增加基础代码。

---
+++ src/main/java/com/rrdssfgcs/interviewcheatsheet/data/BaseSpider.java
@@ -0,0 +1,10 @@
+package com.rrdssfgcs.interviewcheatsheet.data;
+
+/**
+ * 数据抓取抽象类
+ *
+ * @author parker
+ * @time 2017-09-29 21:56
+ **/
+public class BaseSpider {
+}
---
+++ src/main/java/com/rrdssfgcs/interviewcheatsheet/engine/BaseEngineUnit.java
@@ -0,0 +1,10 @@
+package com.rrdssfgcs.interviewcheatsheet.engine;
+
+/**
+ * 引擎基础抽象类
+ *
+ * @author parker
+ * @time 2017-09-29 21:55
+ **/
+public class BaseEngineUnit {
+}
