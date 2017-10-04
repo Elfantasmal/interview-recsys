e97dca458a3b8ba26e94615bbe9f8ba2d26a0889
xjwsfgs <xjwsfgcs_min@163.com>
2017-10-04 18:04:08 +0800

    MOD:1.利用jsoup的.attr()获取超链接。

--- src/main/java/com/rrdssfgcs/interviewcheatsheet/data/spider/NowcoderSpider.java
+++ src/main/java/com/rrdssfgcs/interviewcheatsheet/data/spider/NowcoderSpider.java
@@ -5,11 +5,15 @@ import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+//获取baseUrl的链接加入的包
 
 /**
  * 牛客网抓取类
@@ -20,14 +24,19 @@ import java.util.List;
 
 public class NowcoderSpider extends BaseSpider {
 
+    @Override
     public void update() {
 
         List<String> urls = getUrls("");
         if (!CollectionUtils.isEmpty(urls)) {
-            urls.forEach(u -> saveArticleToDatabase(getArticleFromUrl(u)));
+//            urls.forEach(u -> saveArticleToDatabase(getArticleFromUrl(u)));
+            for (String str : urls) {
+                saveArticleToDatabase(getArticleFromUrl(str));
+            }
         }
     }
 
+    @Override
     public Article getArticleFromUrl(String url) {
         Article article = new Article();
         if (StringUtils.isBlank(url)) {
@@ -41,22 +50,37 @@ public class NowcoderSpider extends BaseSpider {
         }
 
         if (doc != null) {
-            article.setTitle(doc.select("").text());
-            //TODO
-//            article.setContent();
-//            article.setContent();
-//            article.setContent();
-//            article.setContent();
-//            article.setContent();
-//            article.setUpdateTime(LocalDate.now());
+            article.setTitle(doc.select("title").text());
+//            article.setTitle(doc.title());
+            article.setContent(doc.select("div.post-topic-des").text());
+            article.setAuthor(doc.select("a[data-card-uid]").text());
+            article.setSource("nowcoder");
+            article.setUrl(doc.baseUri());
+            Date day = new Date();
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//                System.out.println(df.format(day));
+            article.setInsertTime(df.format(day));
+            article.setUpdateTime(doc.select("span.post-time").text());
         }
         return article;
     }
 
-    private List<String> getUrls(String baseUrl) {
+    public List<String> getUrls(String baseUrl) {
         List<String> urls = new ArrayList<>();
         //TODO
-        urls.add("asdasd");
+
+        Document doc1 = null;
+        try {
+            doc1 = Jsoup.connect(baseUrl).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //从获得的网页代码中找出第一个“面经”的链接地址
+        Element doc2 = null;
+        doc2 = doc1.select("div.discuss-main").get(0);//找到相关元素串
+        Element link = doc2.select("a").first();//查找第一个a元素
+        String linkHref = "www.nowcoder.com" + link.attr("href"); // 取得链接地址
+        urls.add(linkHref);
         return urls;
     }
 
--- src/test/java/com/rrdssfgcs/interviewcheatsheet/data/spider/NowcoderSpiderTest.java
+++ src/test/java/com/rrdssfgcs/interviewcheatsheet/data/spider/NowcoderSpiderTest.java
@@ -6,9 +6,16 @@ public class NowcoderSpiderTest {
     private NowcoderSpider nowcoderSpider = new NowcoderSpider();
 
     @Test
+    public void getUrls() throws Exception {
+        System.out.println(nowcoderSpider.getUrls("https://www.nowcoder.com/discuss/community/645?type=2"));
+    }
+
+
+    @Test
     public void getArticleFromUrl() throws Exception {
         //TODO
         System.out.println(nowcoderSpider.getArticleFromUrl("http://www.nowcoder.com/discuss/52113?type=2&order=0&pos=9&page=1").toString());
     }
+
 
 }
