5f83a7cb456115c9a50ca5891bf28ccd54b53f9e
xjwsfgcs <32239733+xjwsfgcs@users.noreply.github.com>
2017-12-12 20:18:06 +0800

    MOD:完善了利用线性回归进行预测的代码，使用diabetes的数据获得了初步的预测结果，与原始python的例子基本吻合。

--- src/main/java/com/rrdssfgcs/interviewcheatsheet/linearregression/NormalEquationPredictor.java
+++ src/main/java/com/rrdssfgcs/interviewcheatsheet/linearregression/NormalEquationPredictor.java
@@ -2,13 +2,16 @@ package com.rrdssfgcs.interviewcheatsheet.linearregression;
 
 import Jama.Matrix;
 import com.rrdssfgcs.interviewcheatsheet.utils.CSVUtils;
-
 import java.util.List;
 
 public class NormalEquationPredictor {
     public static void main(String[] args){
-        /*TODO定义一个训练输入features，定义相应的values；
-       还有测试features；
+       /**
+       读入数据，并构建相应的训练参数数组：
+       * featuresTrain
+       * valuesTrain
+       构建测试参数：
+       * featuresTest
         */
         String csvFilePath = "./data/X.csv";
         char spliter = ' ';
@@ -17,28 +20,76 @@ public class NormalEquationPredictor {
             System.out.println("featuresList error");
             return;
         }
-        double[][] features = new double[featuresList.size()][1];
-        for (int i = 0; i < featuresList.size(); i++) {
-            features[i][0] = featuresList.get(i).get(0);
+
+        int trainSizeRow=featuresList.size()-20;
+        int trainSizeCol=2;
+        double[][] featuresTrain = new double[trainSizeRow][trainSizeCol];
+        for (int i = 0; i < trainSizeRow; i++) {
+            featuresTrain[i][0] = 1.0;
+            featuresTrain[i][1] = featuresList.get(i).get(2);
         }
+
+        int testSizeRow=20;
+        int testSizeCol=2;
+        double[][] featuresTest = new double[testSizeRow][testSizeCol];
+        for (int i =(featuresList.size()-20) ; i<featuresList.size(); i++) {
+            featuresTest[i-(featuresList.size()-20)][0] = 1.0;
+            featuresTest[i-(featuresList.size()-20)][1] = featuresList.get(i).get(2);
+        }
+        /*
+        打印featuresTrain数组查看
+        for (int i = 0; i < featuresTrain.length; i++) {
+            for (int j = 0; j < featuresTrain[i].length; j++) {
+                System.out.println(featuresTrain[i][j]);
+            }
+        }
+        */
+
         csvFilePath = "./data/y.csv";
         List<List<Double>> valuesList = CSVUtils.readeCSV(csvFilePath, spliter);
+
         if (valuesList == null) {
             System.out.println("valuesList error");
             return;
         }
-        double[][] values = new double[valuesList.size()][1];
-        for (int i = 0; i < valuesList.size(); i++) {
-            values[i][0] = valuesList.get(i).get(0);
+        int valuesTrainSizeRow=valuesList.size()-20;
+        int valuesTrainSizeCol=1;
+        double[][] valuesTrain = new double[valuesTrainSizeRow][valuesTrainSizeCol];
+        for (int i = 0; i < valuesTrainSizeRow; i++) {
+            valuesTrain[i][0] = valuesList.get(i).get(0);
         }
-
-        Matrix featuresMatrix = new Matrix(features);
-        Matrix valuesMatrix = new Matrix(values);
-
-        Matrix wT = normalEquation(featuresMatrix, valuesMatrix);
-
-//        Matrix predictorValues=predictor(wt,testFeatures);
-//        System.out.println(predictorValues);
+        /*
+        double[][] valuesTest = new double[20][1];
+        for (int i = valuesList.size()-20; i < valuesList.size(); i++) {
+            valuesTest[i-422][0] = valuesList.get(i).get(0);
+        }
+        */
+        /*
+        打印valuesTrain数组
+        for (int i = 0; i < valuesTrain.length; i++) {
+            for (int j = 0; j < valuesTrain[i].length; j++) {
+                System.out.println(valuesTrain[i][j]);
+            }
+        }
+        */
+        /**
+         * 构建相应的训练和测试矩阵，计算线性回归的关键参数w，并调用predictor方法进行预测。
+         */
+        Matrix featuresTrainMatrix = new Matrix(featuresTrain);
+        Matrix valuesTrainMatrix = new Matrix(valuesTrain);
+        Matrix featuresTestMatrix = new Matrix(featuresTest);
+        Matrix w = normalEquation(featuresTrainMatrix, valuesTrainMatrix);
+        Matrix predictorValues=predictor(w,featuresTestMatrix);
+        double[][] predValues=predictorValues.getArrayCopy();
+        /*
+        打印predValues数组
+         */
+        System.out.println("predValues");
+        for (int i = 0; i < predValues.length; i++) {
+            for (int j = 0; j < predValues[i].length; j++) {
+                System.out.println(i+"  "+predValues[i][j]);
+            }
+        }
     }
 
     public static Matrix normalEquation(Matrix features, Matrix values) {
@@ -47,12 +98,11 @@ public class NormalEquationPredictor {
         Matrix pseudoinverse=featuresTransposeTimeFeatures.inverse();
         Matrix featuresTransposeTimeValues = featuresTranspose.times(values);
         Matrix w = pseudoinverse.times(featuresTransposeTimeValues);
-        Matrix wTranspose=w.inverse();
-        return wTranspose;
+        return w;
     }
 
-    public static Matrix predictor(Matrix wT, Matrix testFeatures) {
-        Matrix predictorValues = wT.times(testFeatures);
+    public static Matrix predictor(Matrix w, Matrix featuresTest) {
+        Matrix predictorValues = featuresTest.times(w);
         return predictorValues;
     }
 }
